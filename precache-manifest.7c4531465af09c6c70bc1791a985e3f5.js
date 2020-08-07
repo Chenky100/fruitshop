8ebc4d7df4fbb433f1e4746f4dac5b41bab6598c
Chenky100 <chenky100@gmail.com>
2020-08-08 00:49:03 +0300

    Updates

---
+++ precache-manifest.7c4531465af09c6c70bc1791a985e3f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9d27a608875c9f2b496fb99ce40d1d",
+    "url": "/fruitshop/index.html"
+  },
+  {
+    "revision": "e7d8b7f2f2f4bd26a3e9",
+    "url": "/fruitshop/static/css/main.121c6396.chunk.css"
+  },
+  {
+    "revision": "e0fe3d0bec1a0f2cdf5c",
+    "url": "/fruitshop/static/js/2.3cbe231d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fruitshop/static/js/2.3cbe231d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7d8b7f2f2f4bd26a3e9",
+    "url": "/fruitshop/static/js/main.1573e3e2.chunk.js"
+  },
+  {
+    "revision": "c3e1f44866d16181fff5",
+    "url": "/fruitshop/static/js/runtime-main.246942a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fruitshop/static/media/logo.5d5d9eef.svg"
+  }
+]);
